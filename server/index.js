a995843a02744fedf19b800fc5f0f14d1d16f7b3
Daniel Rosel <daniel@alves.world>
2023-09-24 15:21:31 +0200

    functional logins

--- server/index.js
+++ server/index.js
@@ -5,39 +5,78 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
+var Spotify = require('spotify-web-api-js');
+const axios = require('axios');
 
 const routes = [
     {
         method: 'GET',
-        path: '/spotify/auth/callback',
+        path: '/spotify/auth',
         handler: async (request, res) => {
-            const { code } = request.query;
             const client_id = process.env.CLIENT_ID;
-            const client_secret = process.env.CLIENT_SECRET;
-            const redirect_uri = process.env.REDIRECT_URI;
-            const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-            const spotify = new Spotify();
-            const { access_token, refresh_token } = await spotify.authorizationCodeGrant(code);
-            const user = await spotify.getMe(access_token);
-            const { id, display_name, email } = user.body;
-            const newUser = {
-                spotifyId: id,
-                displayName: display_name,
-                email,
-                accessToken: access_token,
-                refreshToken: refresh_token
-            };
-            const updateResponse = await db.collection('users').updateOne(
-                { spotifyId: id },
-                { $set: newUser },
-                { upsert: true }
-            );
-            res.json({
-                message: 'success',
-                data: newUser
+            const { event } = request.query;
+            const redirect_uri = "http://localhost:3000/spotify/auth/callback/"+event;
+            let scopes = [
+                'user-read-private',
+                'user-read-email',
+                'user-library-read',
+                'user-top-read'
+            ].join(' ');
+            res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
+        }
+    },
+    {
+        path: '/spotify/auth/callback/:event',
+        method: 'GET',
+        handler: async (request, res) => {
+            console.log(request.query);
+            const { code } = request.query;
+            const { event } = request.params;
+            // const client_id = process.env.CLIENT_ID;
+            // const client_secret = process.env.CLIENT_SECRET;
+            // auth with spotify
+            const auth = axios.create({
+                baseURL: 'https://accounts.spotify.com/api/token',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
+                },
+                params: {
+                    grant_type: 'authorization_code',
+                    code,
+                    redirect_uri: 'http://localhost:3000/spotify/auth/callback/' + event
+                },
+                method: 'POST'
             });
+            const { data } = await auth();
+            console.log(data);
+            // get user info via http
+            const userData = axios.create({
+                baseURL: 'https://api.spotify.com/v1/me',
+                headers: {
+                    'Authorization': 'Bearer ' + data.access_token
+                },
+                method: 'GET'
+            });
+            let { data: user } = await userData();
+            // save user to db
+            user = {
+                ...user,
+                ...data,
+                'signupDate': new Date(),
+                'event': event
+            }
+            // connect to db
+            await client.connect();
+            const db = client.db('harmoniQ');
+            await db.collection('users').insertOne(user);
+            await client.close();
+            res.json(
+                {
+                    'message': 'success',
+                    'user': user
+                }
+            )
         }
     },
     {
