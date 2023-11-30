4781cebffa0031ab885d3f6d743935ab13fa40a5
velocitatem <daniel@alves.world>
2023-11-30 12:47:09 +0100

    re-something-stuff

---
+++ processing/index.js
@@ -0,0 +1,316 @@
+const axios = require('axios');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+// const port = process.env.PORT || 3000;
+
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const event = 'extraqueso'
+
+// get all the users
+const getUsers = async () => {
+    try {
+        await client.connect();
+        const db = client.db('harmoniQ');
+        const users = db.collection('users');
+        let data = await users.find({ 'event': event
+                                    }).toArray();
+        client.close();
+        // there might be duplicates of users, so we filter them out by getting the latest one by signupDate
+        data = data.sort((a, b) => new Date(b.signupDate) - new Date(a.signupDate)); // sort by signupDate
+        // filter out duplicates
+        const seen = new Set();
+        data = data.filter(user => {
+            const duplicate = seen.has(user.id);
+            seen.add(user.id);
+            return !duplicate;
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const fs = require('fs');
+const saveJSON = (data, name) => {
+    fs.writeFile(name+".json", JSON.stringify(data), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+
+/*
+
+const params = {
+  'limit': 20,
+  'market': 'ES',  // Replace with the desired market/country code
+  'seed_artists': '4NHQUGzhtTLFvgF5SZesLK',  // Replace with seed artists
+  'target_danceability': 0.690,
+  'min_danceability': 0.323,
+  'max_danceability': 0.874,
+  'target_energy': 0.665,
+  'min_energy': 0.31,
+  'max_energy': 0.911,
+  'target_loudness': -8.33,
+  'min_loudness': -14.821,
+  'max_loudness': -3.646,
+  'target_speechiness': 0.063,
+  'min_speechiness': 0.0268,
+  'max_speechiness': 0.189,
+  'target_acousticness': 0.135,
+  'min_acousticness': 0.00134,
+  'max_acousticness': 0.793,
+  'target_instrumentalness': 0.185,
+  'min_instrumentalness': 0.0,
+  'max_instrumentalness': 0.904,
+  'target_liveness': 0.151,
+  'min_liveness': 0.0293,
+  'max_liveness': 0.508,
+  'target_valence': 0.576,
+  'min_valence': 0.135,
+  'max_valence': 0.962,
+  'target_tempo': 124.81,
+  'min_tempo': 88.825,
+  'max_tempo': 188.155,
+  'target_key': 2,
+  'min_key': 0,
+  'max_key': 11,
+  'target_mode': 0,
+  'min_mode': 0,
+  'max_mode': 1
+};
+  */
+const computeParams = (features) => {
+  const audioFeatures = features.audio_features;
+
+  // Initialize variables to hold sum and count for each feature
+  const sum = {}, count = {}, min = {}, max = {};
+  audioFeatures.forEach(feature => {
+    for (const [key, value] of Object.entries(feature)) {
+      // Skip non-numeric and irrelevant features
+      if (typeof value !== 'number' || ['type', 'id', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature'].includes(key)) {
+        continue;
+      }
+      sum[key] = (sum[key] || 0) + value;
+      count[key] = (count[key] || 0) + 1;
+      min[key] = min[key] !== undefined ? Math.min(min[key], value) : value;
+      max[key] = max[key] !== undefined ? Math.max(max[key], value) : value;
+    }
+  });
+
+  // Calculate averages
+  const avg = {};
+  for (const key in sum) {
+    avg[key] = sum[key] / count[key];
+  }
+
+  // Construct the params object
+  const params = {
+    limit: 20,
+    market: 'US',  // Replace with the desired market/country code
+    seed_artists: '4NHQUGzhtTLFvgF5SZesLK'  // Replace with seed artists
+  };
+
+  for (const key in avg) {
+    params[`target_${key}`] = avg[key];
+    params[`min_${key}`] = min[key];
+    params[`max_${key}`] = max[key];
+  }
+
+  return params;
+};
+
+
+const validateToken = async (user) => {
+
+    const testToken = axios.create({
+        baseURL: 'https://api.spotify.com/v1/me',
+        headers: {
+            'Authorization': 'Bearer ' + user.access_token
+        },
+        method: 'GET'
+    });
+    // get status code
+    let status;
+    try {
+        let { status: statusCode } = await testToken();
+        status = statusCode;
+    } catch (error) {
+        status = error.response.status;
+    }
+    console.log(status);
+    if (status !== 200) {
+        // refresh token
+        const refreshToken = axios.create({
+            baseURL: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            params: {
+                grant_type: 'refresh_token',
+                refresh_token: user.refresh_token
+            },
+            auth: {
+                username: process.env.CLIENT_ID,
+                password: process.env.CLIENT_SECRET
+            },
+            method: 'POST'
+        });
+        let { data: newToken } = await refreshToken();
+        console.log(newToken);
+        // update user in db
+        await client.connect();
+        const db = client.db('harmoniQ');
+        const users = db.collection('users');
+        await users.updateOne({ 'id': user.id, 'signupDate': user.signupDate
+                                }, { $set: { 'access_token': newToken.access_token } });
+        await client.close();
+        user = { ...user, 'access_token': newToken.access_token }; // update user
+    }
+    return user;
+}
+
+
+const getTopTracksForUser = async (user) => {
+
+        // get top tracks
+        const topTracks = axios.create({
+            baseURL: 'https://api.spotify.com/v1/me/top/tracks',
+            headers: {
+                'Authorization': 'Bearer ' + user.access_token
+            },
+            method: 'GET'
+        });
+        let { data: tracks } = await topTracks();
+        return tracks;
+    }
+
+
+const getAPPToken = async () => {
+    // get token from client id and secret
+    const auth = axios.create({
+        baseURL: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        params: {
+            grant_type: 'client_credentials'
+        },
+        auth: {
+            username: process.env.CLIENT_ID,
+            password: process.env.CLIENT_SECRET
+        },
+        method: 'POST'
+    });
+    const { data: token } = await auth();
+    console.log(token);
+    return token;
+}
+
+const getFeaturesForTracks = async (trackIds, token) => {
+    const features = axios.create({
+        baseURL: 'https://api.spotify.com/v1/audio-features?ids=' + trackIds.join(','),
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        method: 'GET'
+    });
+    let { data: featuresData } = await features();
+    return featuresData;
+}
+
+const getTrackGenres = async (trackIds, token) => {
+    // split trackIds into chunks of 49
+    let artistIDs= [];
+    while (trackIds.length > 0) {
+        artistIDs.push(trackIds.splice(0, 49));
+    }
+    // get genres for each chunk
+    let trackData = await Promise.all(artistIDs.map(async (ids) => {
+        const artists = axios.create({
+            baseURL: 'https://api.spotify.com/v1/artists?ids=' + ids.join(','),
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            method: 'GET'
+        });
+        let { data: artistsData } = await artists();
+        // get genres
+        let genres = artistsData.artists.map(artist => artist.genres);
+        return genres;
+    }));
+    // get track data
+    trackData = trackData.flat();
+    return trackData;
+}
+
+
+
+
+getUsers().then(async (data) => {
+    console.log(data);
+
+    let token = await getAPPToken();
+    let allTracks = [];
+    // async itterate over all users
+    Promise.all(data.map(async (user) => {
+        user = await validateToken(user);
+        console.log(user);
+        let tracks = await getTopTracksForUser(user);
+        allTracks = [...allTracks, tracks.items];
+    })
+    ).then(async () => {
+        allTracks = allTracks.flat();
+        allTracks = allTracks.filter((track, index, self) =>
+            index === self.findIndex((t) => (
+                t.id === track.id
+            ))
+        )
+        // uniqe track ids
+        let trackIds = allTracks.map(track => track.id);
+        let trackArtists = allTracks.map(track => track.artists[0].id);
+
+        let trackData = await getTrackGenres(trackArtists, token.access_token);
+        saveJSON(trackData, 'trackData');
+        let matchGenre = "reggaeton,latin,hip-hop,party,pop".split(',');
+        let filteredTracks = [];
+        for (let i = 0; i < trackData.length; i++) {
+            let genres = trackData[i];
+            // if one of maches is in genres
+            let match = false;
+            genres.forEach(genre => {
+                matchGenre.forEach(match => {
+                    console.log(genre, match);
+                    if (genre.includes(match)) {
+                        console.log('match');
+                        match = true;
+                        filteredTracks.push(trackIds[i]);
+                        return;
+                    }
+                })
+            })
+        }
+        allTracks = [...new Set(filteredTracks)];
+        // remove duplicates
+        // filter out those that have
+
+        saveJSON(allTracks, 'tracks');
+    }).then(async () => {
+        let features = await getFeaturesForTracks(allTracks, token.access_token);
+        let params = computeParams(features);
+        saveJSON(params, 'params');
+    });
+
+
+
+
+
+
+    // get audio features for all tracks
+
+}).catch((err) => {
+            console.log(err);
+        }
+    );
--- src/dj_alpha/passive_dj.py
+++ src/dj_alpha/passive_dj.py
@@ -9,20 +9,15 @@ directory = "/home/velocitatem/Daily Mix 3/"
 songs = os.listdir(directory)
 tracks = []
 
-import random
-random.shuffle(songs)
-songs = songs[:5]
-
 for song in songs:
     y, sr = librosa.load(directory + song)
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     print(song, tempo)
-    # subtract bf[1] - bf[0] to get the length of the first beat and so on for all
-    diffs = [beat_frames[i + 1] - beat_frames[i] for i in range(len(beat_frames) - 1)]
-    tracks.append((song, tempo, beat_frames, diffs))
+    tracks.append((song, tempo, beat_frames))
 
 # order the tracks by tempo
 tracks.sort(key=lambda x: x[1])
+
 
 print(tracks)
 
@@ -31,34 +26,33 @@ playback = [] # track, start time, end time
 def beat_match_tracks(tracks):
     playback = []
 
-    for t in range(len(tracks)):
-        # get the track and the next track
-        track = tracks[t]
-        next_track = tracks[t + 1] if t < len(tracks) - 1 else None
+    # Assume the first track's tempo as ground tempo
+    ground_tempo = tracks[0][1]
 
-        # get the beat frames for the track
-        track_beat_frames = track[2]
-        next_track_beat_frames = next_track[2] if next_track else None
+    # Initialize start_time
+    start_time = 0
 
-        # first place in first track to stop and start the next track
-        # look at 70% of the way through the track and past to find a beat
-        # that matches the next track
-        for i in range(int(len(track_beat_frames) * 0.7), len(track_beat_frames)):
-            # get the beat frame
-            beat_frame = track_beat_frames[i]
+    for i in range(len(tracks)):
+        song, tempo, beat_frames = tracks[i]
 
-            # check if the next track has a beat frame that matches
-            # the current track
-            if next_track_beat_frames and beat_frame in next_track_beat_frames:
-                # get the start and end times for the track
-                start_time = librosa.frames_to_time(beat_frame)
-                end_time = librosa.frames_to_time(next_track_beat_frames[next_track_beat_frames.index(beat_frame) + 1]) if next_track_beat_frames.index(beat_frame) < len(next_track_beat_frames) - 1 else librosa.get_duration(y)
+        # Calculate the speed change factor to match the ground tempo
+        speed_change = ground_tempo / tempo
 
-                # add the track to the playback
-                playback.append((track[0], start_time, end_time))
+        # Calculate new beat_frames to match the ground tempo
+        adjusted_beat_frames = beat_frames * speed_change
 
-                # break out of the loop
-                break
+        # Find the closest beat frame to start
+        closest_start_frame = min(adjusted_beat_frames, key=lambda x:abs(x-start_time))
+
+        # Assuming we will play for 30 seconds from the closest_start_frame
+        end_time = closest_start_frame + 30  # You can adjust this
+
+        # Add to the playback list
+        playback.append((song, closest_start_frame, end_time))
+
+        # Update start_time for next iteration
+        start_time = end_time
+
     return playback
 
 # TODO beat match the tracks
